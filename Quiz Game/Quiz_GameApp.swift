7830803ae41c96baf6857a0841b6597663e77e7d
Sunny Feng <sunny.feng@xero.com>
2022-06-20 09:35:06 +1200

    Initial Commit

---
+++ Quiz Game/Quiz_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_GameApp.swift
+//  Quiz Game
+//
+//  Created by Sunng Feng on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
